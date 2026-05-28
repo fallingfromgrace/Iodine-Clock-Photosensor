f5bac3c6a2313f322279cb575f3df06209eaec74
Ethan Yamada <ethanhyamada@gmail.com>
2026-05-27 23:21:58 -0500

    initial code comparing adafruit sensor light to preset threshold

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//TODO: setup code to display current lightlevel on LCD, ensure no memory leaks, 
+//how to use to toggle switch?
+
+
+const int out = 8; //output pin
+const int sensorPin = A0; // analog pin
+int sensorValue = 0; // current value of sensor
+const int threshold = 500; // threshold
+
+
+//function declarations
+void compareLight_threshold(int sp); //takes sensor pin and compares to global thresh
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(out, OUTPUT); //out stores output
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  compareLight_threshold(sensorPin);
+  Serial.println(sensorValue);
+  delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+//fun defs
+void compareLight_threshold(int sp) {
+  sensorValue = analogRead(sp);
+  if (sensorValue < threshold) {
+    digitalWrite(out, HIGH);
+  }
+  else {
+    digitalWrite(out, LOW);
+  }
 }
